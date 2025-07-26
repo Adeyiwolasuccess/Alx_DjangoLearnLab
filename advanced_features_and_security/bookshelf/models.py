fa0fc147d5738b80389f6c23587eadde585afe2a
adeyiwola success <successadeyiwola@gmail.com>
2025-07-26 05:30:37 -0700

    Implement CustomUser model extending AbstractUser with additional fields

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,19 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    def __str__(self):
-        return self.username
-
-    class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
-
-
-# Your existing Book model 
+# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
