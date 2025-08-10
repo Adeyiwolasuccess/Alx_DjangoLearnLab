55503054488af35cde40bdfef518bb148f1f0e63
adeyiwola success <successadeyiwola@gmail.com>
2025-08-10 08:40:00 -0700

    Implement filtering, searching, and ordering for Book API

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters
+from django_filters import rest_framework 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
