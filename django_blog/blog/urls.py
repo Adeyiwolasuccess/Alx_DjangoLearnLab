8c9fca22d0160adb3306dd7fba16d5da6f18642f
adeyiwola success <successadeyiwola@gmail.com>
2025-08-16 13:41:21 -0700

    Add blog post CRUD, templates, and forms

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,7 +9,6 @@ from .views import (
     PostDeleteView
 )
 
-
 urlpatterns = [
     # Functional views
     path('', views.home, name='home'),
@@ -17,9 +16,13 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 
     # Post CRUD views
-    path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/', PostListView.as_view(), name='posts'),  # list view stays plural
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # detail view stays plural
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # Auth
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
