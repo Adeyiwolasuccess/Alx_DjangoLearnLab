52b14a548adc67625e9f85c8b1d4c42f63668a52
adeyiwola success <successadeyiwola@gmail.com>
2025-08-24 04:30:38 -0700

    Setup Social Media API with custom user model, auth views, and serializers

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,18 +2,25 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
-from .models import User
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({"user": response.data, "token": token.key})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)  # token already created in serializer
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token.key
+        })
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -21,7 +28,11 @@ class LoginView(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
         token, _ = Token.objects.get_or_create(user=user)
-        return Response({"user": UserSerializer(user).data, "token": token.key})
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token.key
+        })
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
